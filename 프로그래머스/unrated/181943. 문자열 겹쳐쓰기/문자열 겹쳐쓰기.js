10f6d6aa3a041b8eea8f6575636b7c02cbd8e0af
chasura416 <49605210+chasura416@users.noreply.github.com>
2023-07-09 13:31:04 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,7 @@
+function solution(my_string, overwrite_string, s) {
+    let answer = [];
+    my_string = my_string.split("");
+    my_string.splice(s, overwrite_string.length, overwrite_string);
+    answer = my_string.join("");
+    return answer;
+}
