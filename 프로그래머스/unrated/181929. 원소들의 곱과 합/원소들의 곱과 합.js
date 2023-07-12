d0178d1f940c9d477c9e03b9a5a3572673b94783
chasura416 <49605210+chasura416@users.noreply.github.com>
2023-07-13 01:05:43 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let res1 = 0;
+    let res2 = 1;
+    for(let i=0; i<num_list.length; i++) {
+        res1 += num_list[i];
+    }
+    for(let i=0; i<num_list.length; i++) {
+        res2 *= num_list[i];
+    } 
+    return res2 < res1**2 ? 1 : 0;
+}
