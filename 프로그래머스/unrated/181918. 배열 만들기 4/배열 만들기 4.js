6b5d0cff3a952d318a4bf0147c6c443487b718c6
chasura416 <49605210+chasura416@users.noreply.github.com>
2023-07-22 00:19:30 +0900

    [unrated] Title: 배열 만들기 4, Time: 3.04 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,32 @@
+// function solution(arr) {
+//     let stk = [];
+//     for(let i = 0; i < arr.length;){
+//         if(stk.length === 0) {
+//             stk.push(arr[i]);
+//             i++;
+//         }
+//         else if(stk[stk.length-1] <= arr[i]) {
+//             stk.push(arr[i]);
+//             i++;
+//         }
+//         else if(stk[stk.length-1] >= arr[i]) {
+//             stk.pop();
+//         }
+//     }
+//     return stk;
+// }
+
+function solution(arr) {
+  let stk = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (stk.length === 0 || stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+    } else if (stk[stk.length - 1] >= arr[i]) {
+      while (stk.length > 0 && stk[stk.length - 1] >= arr[i]) {
+        stk.pop();
+      }
+      stk.push(arr[i]);
+    }
+  }
+  return stk;
+}
