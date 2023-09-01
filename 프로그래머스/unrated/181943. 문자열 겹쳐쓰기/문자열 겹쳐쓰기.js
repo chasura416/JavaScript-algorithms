3f54ebc0aa21a0a4d207d11491f8904f4cdd3a63
chasura416 <49605210+chasura416@users.noreply.github.com>
2023-09-01 23:57:41 +0900

    Update 문자열 겹쳐쓰기.js

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -3,5 +3,5 @@ function solution(my_string, overwrite_string, s) {
     my_string = my_string.split("");
     my_string.splice(s, overwrite_string.length, overwrite_string);
     answer = my_string.join("");
-    return answer;
+    return answer; 
 }
