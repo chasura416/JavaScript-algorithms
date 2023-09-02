d17dc716775bb6de7a3da400fd81cf63070c91eb
chasura416 <49605210+chasura416@users.noreply.github.com>
2023-09-02 23:48:03 +0900

    Update 수열과 구간 쿼리 4.js

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -4,7 +4,7 @@ function solution(arr, queries) {
         for(let j = s; j <= e; j++){
             if(j%k===0){
                 arr[j] += 1;
-            }
+            } 
         }
     }
     return arr;
