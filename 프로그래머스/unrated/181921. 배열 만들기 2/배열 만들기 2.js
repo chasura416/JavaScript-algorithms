173049fa583813b432895718b779c16e34ee69b8
chasura416 <49605210+chasura416@users.noreply.github.com>
2023-10-01 17:29:57 +0900

    Update 배열 만들기 2.js

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -3,7 +3,7 @@ function solution(l, r) {
     for(let i = l; i <= r; i++){
         if(String(i).split('').every(x => x === '5' || x === '0')) {
             answer.push(i);
-        }
+        } 
     }
     return answer.length > 0 ? answer : [-1]; 
 }
